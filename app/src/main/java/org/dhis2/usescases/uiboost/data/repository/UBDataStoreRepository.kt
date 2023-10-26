70628da49074cd1ade2ea0b5c26f535f0e569eb8
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2023-10-26 12:03:59 +0200

    Configuring dataStore for LIsting

--- app/src/main/java/org/dhis2/usescases/uiboost/data/repository/UBDataStoreRepository.kt
+++ app/src/main/java/org/dhis2/usescases/uiboost/data/repository/UBDataStoreRepository.kt
@@ -1,6 +1,14 @@
 package org.dhis2.usescases.uiboost.data.repository
 
+import kotlinx.coroutines.flow.Flow
+import org.dhis2.usescases.uiboost.data.model.DataStoreAppConfig
+import org.dhis2.usescases.uiboost.data.model.media.MediaStoreConfig
+import org.hisp.dhis.android.core.datastore.DataStoreEntry
+
 interface UBDataStoreRepository {
 
     suspend fun downloadDataStore()
+
+    suspend fun getDataStore(): Flow<List<DataStoreEntry>>
+    suspend fun getFilteredMediaDataStore(): Flow<MediaStoreConfig?>
 }
--- app/src/main/java/org/dhis2/usescases/uiboost/viewmodels/HomeProgramViewModel.kt
+++ app/src/main/java/org/dhis2/usescases/uiboost/viewmodels/HomeProgramViewModel.kt
@@ -3,10 +3,14 @@ package org.dhis2.usescases.uiboost.viewmodels
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.processors.PublishProcessor
+import kotlinx.coroutines.Dispatchers
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.dhis2.commons.filters.FilterManager
 import org.dhis2.commons.matomo.MatomoAnalyticsController
 import org.dhis2.commons.schedulers.SchedulerProvider
@@ -16,11 +20,16 @@ import org.dhis2.usescases.main.program.ProgramRepository
 import org.dhis2.usescases.main.program.ProgramView
 import org.dhis2.usescases.main.program.ProgramViewModel
 import org.dhis2.usescases.main.program.StockManagementMapper
+import org.dhis2.usescases.uiboost.data.model.DataStoreAppConfig
+import org.dhis2.usescases.uiboost.data.model.media.MediaStoreConfig
+import org.dhis2.usescases.uiboost.data.repository.UBDataStoreRepository
+import org.hisp.dhis.android.core.datastore.DataStoreEntry
 import timber.log.Timber
 
 class HomeProgramViewModel @Inject internal constructor(
     private val view: ProgramView,
     private val programRepository: ProgramRepository,
+    private val uBoostRepository: UBDataStoreRepository,
     private val schedulerProvider: SchedulerProvider,
     private val filterManager: FilterManager,
     private val matomoAnalyticsController: MatomoAnalyticsController,
@@ -33,6 +42,12 @@ class HomeProgramViewModel @Inject internal constructor(
 
     val _programs = MutableStateFlow<List<ProgramViewModel>>(emptyList())
     val programs: StateFlow<List<ProgramViewModel>> = _programs
+
+    private val _dataStoreDataElement = MutableStateFlow<List<DataStoreEntry>>(emptyList())
+    val dataStoreMedia: StateFlow<List<DataStoreEntry>> = _dataStoreDataElement
+
+    private val _mediaDataStoreFiltered = MutableStateFlow<MediaStoreConfig?>(null)
+    val mediaDataStoreFiltered: StateFlow<MediaStoreConfig?> = _mediaDataStoreFiltered
 
     init {
         getPrograms()
@@ -91,5 +106,19 @@ class HomeProgramViewModel @Inject internal constructor(
                     { Timber.e(it) }
                 )
         )
+    }
+
+    fun getMediaDataStore() {
+        runBlocking(Dispatchers.IO) {
+            launch {
+                uBoostRepository.getDataStore().collectLatest {
+                    _dataStoreDataElement.value = (it)
+                }
+                uBoostRepository.getFilteredMediaDataStore().collectLatest {
+                    _mediaDataStoreFiltered.value = it
+                }
+            }
+        }
+
     }
 }
