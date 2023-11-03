b25ab9e21707d0cdacc174a859d3e4bda806d136
Job Guitiche <job.guitiche@gmail.com>
2023-11-02 23:56:43 -0300

    feat: show error message for invalid media file paths

--- commons/src/main/java/org/dhis2/commons/extensions/ContextExtensions.kt
+++ commons/src/main/java/org/dhis2/commons/extensions/ContextExtensions.kt
@@ -2,11 +2,21 @@ package org.dhis2.commons.extensions
 
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import org.dhis2.commons.R
 import org.dhis2.commons.media.MediaActivity
 
 fun Context.playMedia(mediaUrl: String) {
-    val intent = Intent(this, MediaActivity::class.java)
-    intent.putExtra(MediaActivity.KEY_MEDIA_URL_VALUE, mediaUrl)
-    ContextCompat.startActivity(this, intent, null)
+    if (mediaUrl.isEmpty()) {
+        Toast.makeText(
+            this,
+            getString(R.string.error_message_invalid_file_path),
+            Toast.LENGTH_LONG
+        ).show()
+    } else {
+        val intent = Intent(this, MediaActivity::class.java)
+        intent.putExtra(MediaActivity.KEY_MEDIA_URL_VALUE, mediaUrl)
+        ContextCompat.startActivity(this, intent, null)
+    }
 }
