44083e002d166fddb92fddff5bfde56f4079f390
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2023-11-14 14:32:53 +0200

    Injecting TeiDashboardMobileActivity module

--- app/src/main/java/org/dhis2/usescases/teiDashboard/teiProgramList/TeiProgramListComponent.java
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/teiProgramList/TeiProgramListComponent.java
@@ -1,6 +1,8 @@
 package org.dhis2.usescases.teiDashboard.teiProgramList;
 
 import org.dhis2.commons.di.dagger.PerActivity;
+import org.dhis2.usescases.teiDashboard.TeiDashboardMobileActivity;
+import org.dhis2.usescases.teiDashboard.TeiDashboardModule;
 
 import dagger.Subcomponent;
 
@@ -11,6 +13,8 @@ import dagger.Subcomponent;
 
 @PerActivity
 @Subcomponent(modules = TeiProgramListModule.class)
+//@Subcomponent(modules = {TeiDashboardModule.class, TeiProgramListModule.class})
 public interface TeiProgramListComponent {
     void inject(TeiProgramListActivity activity);
+    void inject(TeiDashboardMobileActivity mobileActivity);
 }
