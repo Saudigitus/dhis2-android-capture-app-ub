fa01542c7ba4302be50b44c36b1b58ef8a5f0e3c
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2023-12-01 14:21:06 +0200

    Create ProgramWithEnrollment.kt

---
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/data/ProgramWithEnrollment.kt
@@ -0,0 +1,47 @@
+package org.dhis2.usescases.teiDashboard.data
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class ProgramWithEnrollment(
+    val programId: String?,
+    val displayName: String?,
+    val programType: String?,
+    val typeName: String?,
+    val enrollmentStatus: Boolean,
+    val countDescription: Int = 0
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readByte() != 0.toByte(),
+        parcel.readInt()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(programId)
+        parcel.writeString(displayName)
+        parcel.writeString(programType)
+        parcel.writeString(typeName)
+        parcel.writeByte(if (enrollmentStatus) 1 else 0)
+        parcel.writeInt(countDescription)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ProgramWithEnrollment> {
+        override fun createFromParcel(parcel: Parcel): ProgramWithEnrollment {
+            return ProgramWithEnrollment(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ProgramWithEnrollment?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
